caa957e1a6bfd56e9e329e352781f19e02388d2c
Wesley Clements <wesley.clements@gmail.com>
2024-08-19 23:51:33 -0400

    added ability to skip main menu animations

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,20 +1,39 @@
 extends Control
 
-var ready_to_play = false
+const ANIMATION_SKIP_DELAY = 0.05
+
+enum UIState {
+	NONE,
+	PLAY_PRESSED,
+	GAME_STARTED,
+}
+
+
+var state = UIState.NONE
 
 @onready var animation_player = $AnimationPlayer as AnimationPlayer
 
 func _process(_delta: float) -> void:
-	if not ready_to_play:
-		return
-	if Input.is_action_just_pressed(&"ui_accept"):
-		ready_to_play = false
-		animation_player.play(&"play")
+	if Input.is_action_just_pressed(&"ui_accept") and animation_player.is_playing() and animation_player.current_animation_position > ANIMATION_SKIP_DELAY:
+		animation_player.speed_scale = 20
+		await animation_player.animation_finished
+		animation_player.speed_scale = 1
+		print(animation_player.speed_scale)
+
+	match state:
+		UIState.PLAY_PRESSED:
+			if Input.is_action_just_pressed(&"ui_accept"):
+				state = UIState.GAME_STARTED
+				animation_player.play(&"play")
+		_:
+			pass
 
 func _on_play_button_pressed() -> void:
-	ready_to_play = true
 	animation_player.play(&"display_controls")
 
+
+func register_play_button_press() -> void:
+	state = UIState.PLAY_PRESSED
 
 func play() -> void:
 	GlobalMessageBus.set_level.emit(0)
